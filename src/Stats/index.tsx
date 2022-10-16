4f68ad1229f52df891b0fc3123ca01eb213f9bfb
wotsushi <wotsushi@gmail.com>
2022-10-16 14:27:22 +0900

    StatsのBodyをindexに統合

--- src/Stats/index.tsx
+++ src/Stats/index.tsx
@@ -1,19 +1,46 @@
-import AWANav from "../AWANav";
-import { useDecks, useResults, useUser } from "../firebase";
-import Body from "./Body";
+import { useState } from "react";
+import { Container } from "react-bootstrap";
+import { Deck, Result } from "result";
+import Detail from "./Detail";
+import Sidebar from "./Sidebar";
+import Summary, { summaryDeck } from "./Summary";
+
+import AWANav from "AWANav";
+import { useDecks, useResults, useUser } from "firebase";
 import "./style.scss";
+import { User } from "firebase/auth";
+
+type Props = {
+  results: Result[];
+  decks: Deck[];
+  user: User | undefined;
+};
+
+const Content = ({ results, decks, user }: Props) => {
+  const [deck, setDeck] = useState<Deck>(summaryDeck);
+  return (
+    <>
+      <AWANav user={user} />
+      <Container>
+        <div className="body" data-testid="content">
+          <Sidebar decks={decks} selectedDeck={deck} setDeck={setDeck} />
+          {deck.name === summaryDeck.name ? (
+            <Summary decks={decks} results={results} />
+          ) : (
+            <Detail results={results} decks={decks} targetDeck={deck} />
+          )}
+        </div>
+      </Container>
+    </>
+  );
+};
 
 const Stats = () => {
   const decks = useDecks();
   const results = useResults();
   const user = useUser();
 
-  return (
-    <>
-      <AWANav user={user} />
-      <Body results={results} decks={decks} />
-    </>
-  );
+  return <Content results={results} decks={decks} user={user} />;
 };
 
 export default Stats;
